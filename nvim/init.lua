3e54b3d1c77d1353ab6453b9d5d6a815654fc6a0
ivfiev <filjko@ya.ru>
2025-10-31 19:59:22 +0000

    csharp inlay hints

--- nvim/init.lua
+++ nvim/init.lua
@@ -801,6 +801,10 @@ require("lazy").setup({
 					},
 				})
 				vim.lsp.config("pyright", { capabilities = capabilities })
+				vim.lsp.config("hls", {
+					capabilities = capabilities,
+					filetypes = { "haskell", "lhaskell", "cabal" },
+				})
 				vim.lsp.config("gopls", {
 					capabilities = capabilities,
 					settings = {
@@ -848,6 +852,20 @@ require("lazy").setup({
 						},
 						["csharp|code_lens"] = {
 							dotnet_enable_references_code_lens = false,
+						},
+						["csharp|inlay_hints"] = {
+							csharp_enable_inlay_hints_for_implicit_object_creation = true,
+							csharp_enable_inlay_hints_for_implicit_variable_types = true,
+							csharp_enable_inlay_hints_for_lambda_parameter_types = true,
+							csharp_enable_inlay_hints_for_types = true,
+							dotnet_enable_inlay_hints_for_indexer_parameters = true,
+							dotnet_enable_inlay_hints_for_literal_parameters = true,
+							dotnet_enable_inlay_hints_for_object_creation_parameters = true,
+							dotnet_enable_inlay_hints_for_other_parameters = true,
+							dotnet_enable_inlay_hints_for_parameters = true,
+							dotnet_suppress_inlay_hints_for_parameters_that_differ_only_by_suffix = true,
+							dotnet_suppress_inlay_hints_for_parameters_that_match_argument_name = true,
+							dotnet_suppress_inlay_hints_for_parameters_that_match_method_intent = true,
 						},
 					},
 				})

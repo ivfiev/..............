1787e11049025b1c651570def9ca67ab54b94439
ivfiev <filjko@ya.ru>
2025-11-17 00:42:02 +0000

    wofi

--- wlogout.sh
+++ wlogout.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-choice=$(echo -e "Off\nReboot\nSuspend" | wofi --dmenu --cache-file=/dev/null)
+choice=$(echo -e "⏻  Off\n⭘  Reboot\n⏼  Suspend" | wofi --dmenu --cache-file=/dev/null)
 
 case $choice in
   "Off") systemctl poweroff;;

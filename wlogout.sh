2f3e7b4f84ea14bf0da138950f65223e05a34043
ivfiev <filjko@ya.ru>
2025-11-17 00:43:48 +0000

    kek

--- wlogout.sh
+++ wlogout.sh
@@ -3,7 +3,7 @@
 choice=$(echo -e "⏻  Off\n⭘  Reboot\n⏼  Suspend" | wofi --dmenu --cache-file=/dev/null)
 
 case $choice in
-  "Off") systemctl poweroff;;
-  "Reboot") systemctl reboot;;
-  "Suspend") systemctl suspend;;
+  "⏻  Off") systemctl poweroff;;
+  "⭘  Reboot") systemctl reboot;;
+  "⏼  Suspend") systemctl suspend;;
 esac

9093b27d91652bde8b8dfbec1c339c12d56806ca
ivfiev <11612131+ivfiev@users.noreply.github.com>
2026-04-21 18:52:09 +0100

    tweak voltages

--- amdgpu.sh
+++ amdgpu.sh
@@ -4,8 +4,8 @@ set -e
 
 if [ "$1" = "--game" ]; then
   echo "applying game profile"
-  sudo tee /sys/class/drm/card1/device/pp_od_clk_voltage <<< "vo -70"
-  sudo tee /sys/class/drm/card1/device/pp_od_clk_voltage <<< "s 1 1800"
+  sudo tee /sys/class/drm/card1/device/pp_od_clk_voltage <<< "vo -80"
+  sudo tee /sys/class/drm/card1/device/pp_od_clk_voltage <<< "s 1 1900"
   sudo tee /sys/class/drm/card1/device/pp_od_clk_voltage <<< "c"
   sudo tee /sys/class/drm/card1/device/power_dpm_force_performance_level <<< manual
   sudo tee /sys/class/drm/card1/device/pp_dpm_mclk <<< 2
@@ -13,8 +13,8 @@ if [ "$1" = "--game" ]; then
   sudo tee /sys/class/drm/card1/device/pp_dpm_fclk <<< 2
 elif [ "$1" = "--compute" ]; then
   echo "applying compute profile"
-  sudo tee /sys/class/drm/card1/device/pp_od_clk_voltage <<< "vo -70"
-  sudo tee /sys/class/drm/card1/device/pp_od_clk_voltage <<< "s 1 1800"
+  sudo tee /sys/class/drm/card1/device/pp_od_clk_voltage <<< "vo -80"
+  sudo tee /sys/class/drm/card1/device/pp_od_clk_voltage <<< "s 1 1900"
   sudo tee /sys/class/drm/card1/device/pp_od_clk_voltage <<< "c"
   sudo tee /sys/class/drm/card1/device/power_dpm_force_performance_level <<< auto
 else

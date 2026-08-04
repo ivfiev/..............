a43b81a18f9495843d7b1fc6aa3caf7ef5338cb3
ivfiev <11612131+ivfiev@users.noreply.github.com>
2026-08-04 23:13:22 +0100

    add disconnect option

--- wofi-mullvad-switch-host.py
+++ wofi-mullvad-switch-host.py
@@ -1,42 +1,43 @@
 import subprocess
 
 try:
-  status = subprocess.check_output(['mullvad', 'status'], text=True)
+    status = subprocess.check_output(["mullvad", "status"], text=True)
 
-  words = status.split()
+    words = status.split()
 
-  prompt = 'Disconnected from VPN!'
-  if words[0] == 'Connected':
-    prompt = f'{words[0]} - {words[2]}, {' '.join(words[-2:])}'
+    prompt = "Disconnected from VPN!"
+    if words[0] == "Connected":
+        prompt = f"{words[0]} - {words[2]}, {' '.join(words[-2:])}"
 
-  relays = subprocess.check_output(['mullvad', 'relay', 'list'], text=True)
+    relays = subprocess.check_output(["mullvad", "relay", "list"], text=True)
 
-  lines = relays.split('\n')
-  country, city, code = '', '', ''
-  wofi = ''
+    lines = relays.split("\n")
+    country, city, code = "", "", ""
+    wofi = ""
 
-  for line in lines:
-    if not line:
-      country, city, code = '', '', ''
-      continue
-    if not line.startswith('\t'):
-      country = line.split(' (')[0].strip()
-    elif line.startswith('\t\t'):
-      if '-wg-' in line:
-        code = line.split()[0].strip()
-        wofi += f'{country}, {city}, {code}\n'
-    elif line.startswith('\t'):
-      city = line.split(' (')[0].strip()
+    for line in lines:
+        if not line:
+            country, city, code = "", "", ""
+            continue
+        if not line.startswith("\t"):
+            country = line.split(" (")[0].strip()
+        elif line.startswith("\t\t"):
+            if "-wg-" in line:
+                code = line.split()[0].strip()
+                wofi += f"{country}, {city}, {code}\n"
+        elif line.startswith("\t"):
+            city = line.split(" (")[0].strip()
+    wofi += "Disconnect\n"
 
-  choice = subprocess.check_output(
-    ['wofi', '--show', 'dmenu', '--prompt', prompt, '-i', '-M', 'fuzzy'],
-    input=wofi,
-    text=True)
-  hostname = choice.split()[-1]
+    choice = subprocess.check_output(["wofi", "--show", "dmenu", "--prompt", prompt, "-i", "-M", "fuzzy"], input=wofi, text=True)
+    hostname = choice.split()[-1]
 
-  if 'Disconnected' in prompt:
-    subprocess.check_output(['mullvad', 'connect'], text=True)
+    if "Disconnect" in choice:
+        subprocess.check_output(["mullvad", "disconnect"])
+    else:
+        if "Disconnected" in prompt:
+            subprocess.check_output(["mullvad", "connect"], text=True)
+        subprocess.check_output(["mullvad", "relay", "set", "location", hostname], text=True)
 
-  subprocess.check_output(['mullvad', 'relay', 'set', 'location', hostname], text=True)
 except Exception as e:
-  subprocess.check_output(['logger', '-t', __file__, str(e)])
+    subprocess.check_output(["logger", "-t", __file__, str(e)])
